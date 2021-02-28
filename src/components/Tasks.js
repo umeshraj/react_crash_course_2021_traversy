e710a1cd14634153fb85ec5acb95ceb642eaf0ab
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-28 05:28:43 -0500

    Using a task list to in Task component

---
+++ src/components/Tasks.js
@@ -0,0 +1,17 @@
+const tasks = [
+  { id: 1, text: "Doc work", day: "Feb 5th at 1:20pm", reminder: true },
+  { id: 2, text: "School work", day: "Apr 1th at 4:20pm", reminder: true },
+  { id: 3, text: "Food shopping", day: "Jan 1th at 3:10pm", reminder: true },
+];
+
+const Tasks = () => {
+  return (
+    <>
+      {tasks.map((task) => {
+        return <h3 key={task.id}>{task.text}</h3>;
+      })}
+    </>
+  );
+};
+
+export default Tasks;
