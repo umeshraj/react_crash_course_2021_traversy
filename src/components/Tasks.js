4bc602a36977dc442e233613b693bb089e97a8e3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-28 05:34:52 -0500

    Usestate hook to make tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,12 @@
-const tasks = [
-  { id: 1, text: "Doc work", day: "Feb 5th at 1:20pm", reminder: true },
-  { id: 2, text: "School work", day: "Apr 1th at 4:20pm", reminder: true },
-  { id: 3, text: "Food shopping", day: "Jan 1th at 3:10pm", reminder: true },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Doc work", day: "Feb 5th at 1:20pm", reminder: true },
+    { id: 2, text: "School work", day: "Apr 1th at 4:20pm", reminder: true },
+    { id: 3, text: "Food shopping", day: "Jan 1th at 3:10pm", reminder: true },
+  ]);
+
   return (
     <>
       {tasks.map((task) => {
