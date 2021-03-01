2030c6aa284508c4c3fdde4af06a4a56d8b9ae4d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-01 05:40:17 -0500

    Move tasks to parent App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Doc work", day: "Feb 5th at 1:20pm", reminder: true },
+    { id: 2, text: "School work", day: "Apr 1th at 4:20pm", reminder: true },
+    { id: 3, text: "Food shopping", day: "Jan 1th at 3:10pm", reminder: true },
+  ]);
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
