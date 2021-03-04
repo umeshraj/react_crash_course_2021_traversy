4ab6767c2b515f7e80ca34c2281e9b32ef4fa0cb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-04 05:30:55 -0500

    Change button color and text for add task

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} />
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
