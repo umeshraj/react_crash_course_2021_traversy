aa3bb7f2b7bb24f68a0fa3d9ca9bb1af0378a114
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-04 05:28:31 -0500

    Pass fn to addtask

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
