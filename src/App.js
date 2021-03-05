f309ba610673a6e3abd4dc6f960514c1d0547a9a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-05 05:30:48 -0500

    Example of loading data in useEffect

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    effect;
-    return () => {
-      cleanup;
+    const fetchTasks = async () => {
+      const res = await fetch("http://localhost:5000/tasks");
+      const data = await res.json();
+      console.log(data);
     };
-  }, [input]);
+    fetchTasks();
+  }, []);
 
   // Add task
   const addTask = (task) => {
